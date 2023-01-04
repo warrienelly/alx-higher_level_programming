fc4e78731e26b05343b9630522984503761d747a
Warrie Warrie <warrie.warrieus@gmail.com>
2023-01-04 23:34:54 +0100

    fix: issues with long line

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,11 @@ if number > 0:
     last_digit = number % 10
 else:
     last_digit = (number*(-1) % 10) * -1
+
+initial_mssg = f'Last digit of {number} is {last_digit} and is'
 if last_digit > 5:
-    print(f'Last digit of {number} is {last_digit} and is greater than 5')
+    print(f'{initial_mssg} greater than 5')
 elif last_digit == 0:
-    print(f'Last digit of {number} is {last_digit} and is 0')
+    print(f'{initial_mssg} 0')
 elif last_digit < 6 and last_digit != 0 and number < 0:
-    print(f'Last digit of {number} is {last_digit} and is less than 6 and not 0')
+    print(f'{initial_mssg} less than 6 and not 0')
