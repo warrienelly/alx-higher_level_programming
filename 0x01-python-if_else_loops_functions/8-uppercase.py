95a0918112538ee13eea98f782882250e89c8f76
Warrie Warrie <warrie.warrieus@gmail.com>
2023-01-08 15:11:19 +0100

    Feat: Used Docstring value

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+def uppercase(str):
+    new_str = ""
+    for chr in str:
+        if ord(chr) in range (97, 123):
+            new_str = new_str + '{:c}'.format((ord(chr) - 32))
+        else:
+           new_str = new_str + chr
+    print(new_str)
