3fca8e7c4ae8de7f28ac3306b72416bd04e0649c
Warrie Warrie <warrie.warrieus@gmail.com>
2023-01-08 15:21:42 +0100

    Fixed: pydocstyle issue

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,8 @@
 def uppercase(str):
     new_str = ""
     for chr in str:
-        if ord(chr) in range (97, 123):
+        if ord(chr) in range(97, 123):
             new_str = new_str + '{:c}'.format((ord(chr) - 32))
         else:
-           new_str = new_str + chr
+            new_str = new_str + chr
     print(new_str)
