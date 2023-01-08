dad0482adfeb8a4a01671e66ed5954b922e9a922
Warrie Warrie <warrie.warrieus@gmail.com>
2023-01-08 15:34:39 +0100

    Fix: Solve pydocstyle issues

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        print((number * (-1)% 10 ), end="")
-        return((number * (-1))% 10 )
+        print((number * (-1) % 10), end="")
+        return((number * (-1)) % 10)
     else:
         print(number % 10, end="")
         return(number % 10)
