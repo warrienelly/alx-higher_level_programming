e6183a43868938f734314566165680c4b0d75112
Warrie Warrie <warrie.warrieus@gmail.com>
2023-01-08 14:15:37 +0100

    Fix: Corrected issues with 89 exception

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 for f1_number in range(0, 10):
     for f2_number in range(0, 10):
-        if f1_number < f2_number:
-            print('{}{}'.format(f1_number, f2_number), end=", ")      
+        if f1_number < f2_number and (f1_number != 8 and f2_number != 9):
+            print('{}{}'.format(f1_number, f2_number), end=", ")
         if f1_number == 8 and f2_number == 9:
             print('{}{}'.format(f1_number, f2_number))
-            
-
