1c6d1fbc0edcb40e43afd6d1f1af1703196b2155
Warrie Warrie <warrie.warrieus@gmail.com>
2023-01-08 22:58:16 +0100

    Fix: Issues with space around operators

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,6 +2,6 @@
 def remove_char_at(str, n):
     new_str = ""
     for chr in range(len(str)):
-        if chr!=n:
+        if chr != n:
             new_str = new_str + str[chr]
     return(new_str)
