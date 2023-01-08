9edf1d624a2b8a0254c79da8279605436ebd6f45
Warrie Warrie <warrie.warrieus@gmail.com>
2023-01-08 14:20:24 +0100

    Fixed: White space issue

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for f1_number in range(0, 10):
     for f2_number in range(0, 10):
-        if f1_number < f2_number and ('{}{}'.format(f1_number, f2_number) != '89') :
+        if f1_number < f2_number and ('{}{}'.format(f1_number, f2_number) != '89'):
             print('{}{}'.format(f1_number, f2_number), end=", ")
         if f1_number == 8 and f2_number == 9:
             print('{}{}'.format(f1_number, f2_number))
