30e3a940fa43b4d5ad22e15b18179f30873a42fb
Warrie Warrie <warrie.warrieus@gmail.com>
2023-01-09 23:24:55 +0100

    Feat: Python is cool

---
+++ 0x02-python-import_modules/101-easy_print.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+from sys import argv
+
+
+def main():
+    print('#pythoniscool')
+
+
+if __name__ == '__main__':
+    main()
