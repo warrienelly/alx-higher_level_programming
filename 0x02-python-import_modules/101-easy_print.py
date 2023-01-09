469eac8e17e467f0d4e7277a65e409df46f4164e
Warrie Warrie <warrie.warrieus@gmail.com>
2023-01-09 23:26:10 +0100

    Fix: Removed other things

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
-from sys import argv
+print('#pythoniscool')
 
 
-def main():
-    print('#pythoniscool')
-
-
-if __name__ == '__main__':
-    main()
