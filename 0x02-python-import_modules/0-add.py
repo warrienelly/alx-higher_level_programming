6ae25461c64598ce7e968487c2ec01038e5f2912
Warrie Warrie <warrie.warrieus@gmail.com>
2023-01-09 20:08:46 +0100

    Feat: import modules

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import add_0
+
+def main():
+    a = 1
+    b = 2
+
+    output = add_0.add(a, b)
+    print('{} + {} = {}'.format(a, b, output))
+
+if __name__ == "__main__":
+    main()
