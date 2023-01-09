269b4c18b334220ef03c36b8417b14f345dc31df
Warrie Warrie <warrie.warrieus@gmail.com>
2023-01-09 22:10:15 +0100

    Fix: Removed the condition filtering 0 args

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,11 +6,11 @@ def main():
     args = sys.argv
     sum_value = 0
 
-    if len(args) != 1:
-        for arg in args[1:]:
-            sum_value = sum_value + int(arg)
+    # if len(args) != 1:
+    for arg in args[1:]:
+        sum_value = sum_value + int(arg)
 
-        print(sum_value)
+    print(sum_value)
 
 
 if __name__ == "__main__":
