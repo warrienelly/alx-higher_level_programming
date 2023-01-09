44a116440000e44eb8e354d9c52697f606895ecf
Warrie Warrie <warrie.warrieus@gmail.com>
2023-01-10 00:11:29 +0100

    Feat: Indexing List

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    ls_len = len(my_list)
+    if idx < 0:
+        return None
+    if idx >= ls_len:
+        return None
+
+    return "{:d}".format(my_list[idx])
