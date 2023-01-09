9590b083568c5f6614e9cd1e38664b7921fecda3
Warrie Warrie <warrie.warrieus@gmail.com>
2023-01-10 00:19:01 +0100

    Fix: Using -1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     ls_len = len(my_list)
     if idx < 0:
         return None
-    if idx >= ls_len:
+    if idx > (ls_len - 1):
         return None
 
     return "{:d}".format(my_list[idx])
