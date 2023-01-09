4c33846b92c7b7cdb0281a48f5c4a47d570a846d
Warrie Warrie <warrie.warrieus@gmail.com>
2023-01-09 20:29:32 +0100

    Fix: Blank lines after defining functions

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 from add_0 import add
 
+
 def main():
     a = 1
     b = 2
 
     output = add(a, b)
     print('{:d} + {:d} = {:d}'.format(a, b, output))
+
 
 if __name__ == "__main__":
     main()
