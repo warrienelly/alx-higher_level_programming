d7a66995a449e4fe7ed17279f69d35d2eceb9820
Warrie Warrie <warrie.warrieus@gmail.com>
2023-01-09 21:43:09 +0100

    Feat: input argument to project

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def main():
+    import sys
+    args = sys.argv
+    if len(args) != 1:
+        print('{} arguments:'.format(len(args) - 1))
+        for index in range(len(args[1:])):
+            print('{}: {}'.format(index + 1, args[index + 1]))
+    else:
+        print('{} arguments.'.format(len(args) - 1))
+
+
+if __name__ == "__main__":
+    main()
