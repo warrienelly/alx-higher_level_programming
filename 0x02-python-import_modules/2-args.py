0862c1e839d560284e47972dadf5c691f9bd8935
Warrie Warrie <warrie.warrieus@gmail.com>
2023-01-09 21:53:14 +0100

    Fix: solving issue with len of arg plural

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,14 @@ def main():
     import sys
     args = sys.argv
     if len(args) != 1:
-        print('{} arguments:'.format(len(args) - 1))
+        if len(args) == 2:
+            print('{} argument:'.format(len(args) - 1))
+        else:
+            print('{} arguments:'.format(len(args) - 1))
         for index in range(len(args[1:])):
             print('{}: {}'.format(index + 1, args[index + 1]))
     else:
-        print('{} argument.'.format(len(args) - 1))
+        print('{} arguments.'.format(len(args) - 1))
 
 
 if __name__ == "__main__":
