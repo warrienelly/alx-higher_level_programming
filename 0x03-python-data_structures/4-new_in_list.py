2c5dfa4cce6506c85b92632f970179d5bad96518
Warrie Warrie <warrie.warrieus@gmail.com>
2023-01-10 22:23:02 +0100

    Fix: Copy from the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,6 @@
 def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()   
     if (idx < 0) or (idx >= len(my_list)):
-        return my_list.copy()
-    my_list[idx] = element
-    return my_list
+        return new_list
+    new_list[idx] = element
+    return new_list
