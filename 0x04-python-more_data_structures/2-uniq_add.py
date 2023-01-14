6fd62980801881fce5bd2a6c586724729bb607a1
Warrie Warrie <warrie.warrieus@gmail.com>
2023-01-14 11:13:02 +0100

    Fix: Add exception for empty list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,6 +2,8 @@
 def uniq_add(my_list=[]):
     sum = 0
     for value in set(my_list):
-        sum = sum + value
-
+        if len(my_list) == 0:
+            return None
+        else:
+            sum = sum + value
     return sum
