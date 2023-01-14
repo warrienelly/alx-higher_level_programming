967ea38aa37952f69c8013b6b378ce83875562f3
Warrie Warrie <warrie.warrieus@gmail.com>
2023-01-14 09:17:51 +0100

    Fix: Adding next line

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-str_value = 'and that piece of art is useful - Dora Korpar, 2015-10-19'
+str_value = 'and that piece of art is useful - Dora Korpar, 2015-10-19\n'
 sys.stderr.write(str_value)
 sys.exit(1)
